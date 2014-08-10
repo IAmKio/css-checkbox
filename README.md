3794a9dab1cb3420d7abf4fec0c5c1fd7b8f2612
Kieran Goodar <kgoodary@gmail.com>
2014-08-10 14:45:02 +0100

    Added bower.json, updated gulping, package.json, read me, src css

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 Circular
 ========
 
-A Javascript library that converts checkboxes to stylable elements using CSS!
+A Javascript library that converts checkboxes to stylable elements using CSS, whilst 
+
+Quick Start
+----------------
+For the TL:DR demon in you
+
+Simply:
+
+	bower install circular
+	
+Then copy or reference the `circular.js` **and** `circular.css` in your HTML from the `dist` directory. **You need to use both files**.
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglifyjs');
 var jshint = require('gulp-jshint');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('default', function() {
 
@@ -16,7 +17,11 @@ gulp.task('default', function() {
     
     gulp.src(['./src/js/*.js'])
         .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('jshint-stylish'));    
-    
+        .pipe(jshint.reporter('jshint-stylish'));  
+        
+          
+    gulp.src('src/css/*.css')
+        .pipe(concatCss("src/css/circular.css"))
+        .pipe(gulp.dest('dist/css'));    
 
 });
